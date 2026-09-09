9daa349cb423aeeba4794dc9552135bdb4572b95
nuclearwarp <59673061+nuclearwarp@users.noreply.github.com>
2026-09-09 15:57:23 +0900

    Integrate native home menu routing (#11)
    * Add native menu router for completed iOS ports
    
    * Fix native zipcode route initializer

---
+++ Maroowell/Home/NativeMenuRouterView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct NativeMenuRouterView: View {
+    let title: String
+    let path: String
+    let session: AppSession
+
+    var body: some View {
+        switch AppAccessPolicy.basePath(path) {
+        case "/zipcode_search":
+            ZipcodeSearchView(session: session)
+        case "/coupangRouteMap.html":
+            RouteEditorView(session: session)
+        case "/coupang_freshbag":
+            FreshbagStatusView(session: session)
+        case "/cleansing_history":
+            CleansingHistoryView(session: session)
+        case "/maroowell_route_info":
+            RouteInfoView(session: session)
+        case "/maroowell_route":
+            RoutePriceView(session: session)
+        case "/dragon_car_index":
+            DragonCarView(session: session)
+        case "/dragon_car_schedule":
+            DragonScheduleView(session: session)
+        case "/admin_access.html":
+            AdminApprovalView(session: session)
+        case "/maroowell_push":
+            PushConsoleView(session: session)
+        default:
+            MaroowellWebView(title: title, path: path)
+        }
+    }
+}
